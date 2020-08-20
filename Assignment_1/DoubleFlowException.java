feac295f080997f3163ecdf2fb895c3dfe67042a
Aurora Schmidt-Brekken Vollvik <aurora.vollvik@roraraptor.no>
2020-08-20 21:55:14 +0200

    Did lots of work on Assignment 1

---
+++ Assignment_1/DoubleFlowException.java
@@ -0,0 +1,16 @@
+package Assignment_1;
+
+class DoubleFlowException extends Exception {
+    
+    /**
+     * This field is only here because the editor complained about it
+     */
+    private static final long serialVersionUID = -1213937452653034074L;
+
+    DoubleFlowException() {
+    }
+
+    DoubleFlowException(String s) {
+        super(s);
+    }
+}
--- Assignment_1/InchesToCentimeters.java
+++ Assignment_1/InchesToCentimeters.java
@@ -1,25 +1,97 @@
 package Assignment_1;
 
+import javax.swing.JOptionPane;
+
 /**
- * InchesToCentimeters
- * -------------------
- * 
- * This class is made for the first assignment of the course IDATT1001
- * Programming 1 at The Norwegian University of Science and Technology.
- * 
- * The program asks the user for an amount in inches and displays the
- * equivalent amount in centimeters.
+ * Asks the user for a number of inches and converts it into centimeters.
  * 
  * @author Aurora Schmidt-Brekken Vollvik
  */
 
 public class InchesToCentimeters {
+
+    private static final String TITLE = "InchesToCentimeters";
+    private static final double CM_PER_INCH = 2.54d; // Factor to scale the input by
+
+    private static boolean validateDouble(double d) {
+
+        boolean validated = true;
+
+        if (d == Double.POSITIVE_INFINITY) { // Positive overflow: d > Double.MAX_VALUE
+            
+            validated = false;
+        }
+        
+        else if (d == Double.NEGATIVE_INFINITY) { // Negative underflow: d < -Double.MAX_VALUE
+            
+            validated = false;
+        }
+
+        else if (Double.compare(+0.0f, d) == 0) { // Negative underflow: -Double.MIN_VALUE < d < 0
+            
+            validated = false;
+        }
+
+        else if (Double.compare(-0.0f, d) == 0) { // Positive underflow: 0 < d < Double.MIN_VALUE
+            
+            validated = false;
+        }
+
+        return validated;
+    }
     public static void main(String[] args) {
 
-        // Ask user for input
+        JOptionPane.showMessageDialog(null, "Welcome!\n\n" + "This little program lets you convert inches to centimeters.", TITLE, JOptionPane.PLAIN_MESSAGE);
 
-        // Use appropriate conversion
+        // Ask the user how many inches they want to convert
+        String input = JOptionPane.showInputDialog(null, "Please enter a number to convert.\n\n" + "Inches:", TITLE, JOptionPane.QUESTION_MESSAGE);
+        double parsedInput = 0.0d;
+        
+        // The control variable starts as false
+        boolean inputValidated = false;
 
-        // Display answer
+        while (input != null) { // Has user already exited/canceled?
+
+            do {
+
+                try { // If no exceptions are thrown, the input is valid and the second statement will be run (except if input evaluates to less than Double.MIN_VALUE)
+
+                    parsedInput = Double.parseDouble(input);
+
+                    if(!validateDouble(parsedInput)) {
+
+                        throw new DoubleFlowException();
+                    }
+
+                    inputValidated = true;
+
+                } catch (NumberFormatException e) { // Illegal input, ask again
+
+                    input = JOptionPane.showInputDialog(null, "Oops! We couldn't interpret that as a number.\n\nPlease only input a number like \"3\" or \"12.4\" without the quotation marks.\n\n" + "Inches:", TITLE, JOptionPane.WARNING_MESSAGE);
+
+                } catch (DoubleFlowException e) {
+
+                    input = JOptionPane.showInputDialog(null, "Oops! Due to technical limitations, we couldn't convert that number.\n\nPlease input a new number like \"3\" or \"12.4\" without the quotation marks.\n\n" + "Inches:", TITLE, JOptionPane.WARNING_MESSAGE);
+
+                }
+
+            } while (input != null && !inputValidated); // Repeat if we still have input and it has not been validated
+
+            if (input != null) { // Check again if user canceled
+                
+                double result = CM_PER_INCH * parsedInput;
+            
+                // Display results
+                JOptionPane.showMessageDialog(null, "Results: " + parsedInput + "\" ➡ " + result + "cm", TITLE, JOptionPane.INFORMATION_MESSAGE);
+
+                // Ask the user for new input. If they enter something, the while loop runs again.
+                input = JOptionPane.showInputDialog(null, "Do you want to run the program again? If so, please enter a new number to convert.\n\n" + "Inches:", TITLE, JOptionPane.QUESTION_MESSAGE);
+                
+            }
+        }
+
+        // Display friendly valediction and exit the program
+        JOptionPane.showMessageDialog(null, "Thank you for using InchesToCentimeters.", TITLE, JOptionPane.PLAIN_MESSAGE);
+
     }
 }
