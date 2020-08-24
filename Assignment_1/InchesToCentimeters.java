848d49413a3972830d4c7fafb9f2f8eb74bbde1a
Aurora Schmidt-Brekken Vollvik <aurora.vollvik@roraraptor.no>
2020-08-24 13:22:04 +0200

    Refactored and fixed bugs

--- Assignment_1/InchesToCentimeters.java
+++ Assignment_1/InchesToCentimeters.java
@@ -11,14 +11,20 @@ import javax.swing.JOptionPane;
 public class InchesToCentimeters {
 
     private static final String TITLE = "InchesToCentimeters";
+    private static final String DEFAULT_MESSAGE = "Please enter a number like \"3\" or \"12.4\" without the quotation marks.\n\nInches:";
     private static final double CM_PER_INCH = 2.54d; // Factor to scale the input by
 
-    private static boolean validateDouble(double d) {
 
-        // TODO Fix: When the user inputs signed or plain zeroes they still get an error.
-        boolean validated = !(Double.isInfinite(d) || Double.compare(+0.0f, d) == 0 || Double.compare(-0.0f, d) == 0);
+    private static String inputInches() {
 
-        return validated;
+        return JOptionPane.showInputDialog(null, DEFAULT_MESSAGE, TITLE, JOptionPane.QUESTION_MESSAGE);
+    }
+    
+    private static String inputInches(String customMessage, int messageType) {
+
+        String message =  customMessage + "\n\n" + DEFAULT_MESSAGE;
+
+        return JOptionPane.showInputDialog(null, message, TITLE, messageType);
     }
 
     public static void main(String[] args) {
@@ -26,8 +32,8 @@ public class InchesToCentimeters {
         JOptionPane.showMessageDialog(null, "Welcome!\n\n" + "This little program lets you convert inches to centimeters.", TITLE, JOptionPane.PLAIN_MESSAGE);
 
         // Ask the user how many inches they want to convert
-        String input = JOptionPane.showInputDialog(null, "Please enter a number to convert.\n\n" + "Inches:", TITLE, JOptionPane.QUESTION_MESSAGE);
-        double parsedInput = 0.0d;
+        String input = inputInches();
+        double parsedInput = 0;
         
         // The control variable starts as false
         boolean inputValidated = false;
@@ -36,37 +42,42 @@ public class InchesToCentimeters {
 
             do {
 
-                try { // If no exceptions are thrown, the input is valid and the second statement will be run (except if input evaluates to less than Double.MIN_VALUE)
+                try {
 
                     parsedInput = Double.parseDouble(input);
 
-                    if (validateDouble(parsedInput)) {
+                    if (!Double.isInfinite(parsedInput)) {
 
-                        inputValidated = true;
+                        inputValidated = true; // We can use the parsed input
                     
                     } else {
 
-                        throw new DoubleFlowException();
+                        String tooLargeMsg = "Oops! This number is too large to compute.";
+
+                        inputValidated = false;
+                        input = null;
+                        input = inputInches(tooLargeMsg, JOptionPane.WARNING_MESSAGE);
                     }
 
                 } catch (NumberFormatException e) { // Illegal input, ask again
 
-                    input = JOptionPane.showInputDialog(null, "Oops! We couldn't interpret that as a number.\n\nPlease only input a number like \"3\" or \"12.4\" without the quotation marks.\n\n" + "Inches:", TITLE, JOptionPane.WARNING_MESSAGE);
+                    String notANumberMsg = "Oops! We couldn't interpret that as a number.";
 
-                } catch (DoubleFlowException e) { // Illegal input, ask again
-
-                    input = JOptionPane.showInputDialog(null, "Oops! Due to technical limitations we can't convert this number.\n\nPlease input a different number like \"3\" or \"12.4\" without the quotation marks.\n\n" + "Inches:", TITLE, JOptionPane.WARNING_MESSAGE);
+                    inputValidated = false;
+                    input = null;
+                    input = inputInches(notANumberMsg, JOptionPane.WARNING_MESSAGE);
 
                 }
 
             } while (input != null && !inputValidated); // Repeat if we still have input and it has not been validated
 
-            // TODO Fix: When the user gets an error earlier in the program, this block runs and gives the associated error message, even when the user has input new, valid data 
+            
             if (input != null) { // Check again if user canceled
                 
                 double result = CM_PER_INCH * parsedInput;
 
-                if (validateDouble(result)) {
+                // Check that the result makes sense
+                if (!Double.isInfinite(result)) {
 
                     String resultMessage = "Results: " + parsedInput + "\" ➡ " + result + "cm";
                     //String formattedResultMessage = String.format("Results: %,#.3f\" ➡ %,#.3fcm", parsedInput, result);
@@ -74,16 +85,20 @@ public class InchesToCentimeters {
                     // Display results
                     JOptionPane.showMessageDialog(null, resultMessage, TITLE, JOptionPane.INFORMATION_MESSAGE);
 
+                    inputValidated = false;
+                    input = null;
+                    input = inputInches("Do you want to run the program again?", JOptionPane.QUESTION_MESSAGE);
+
                 } else {
 
-                    // Error message
-                    JOptionPane.showMessageDialog(null, "Oops! The magnitude of the result is too large or too small (without being zero) to represent in Java's text encoding, so we can't understand it.", TITLE, JOptionPane.INFORMATION_MESSAGE);
+                    String resultTooLargeMessage = "Oops! The magnitude of the result is too large or too small (without being zero) " +
+                                                   "to represent in Java's text encoding, so we can't understand it.";
+
+                    inputValidated = false;
+                    input = null;
+                    input = inputInches(resultTooLargeMessage, JOptionPane.WARNING_MESSAGE);
 
                 }
-
-                // Ask the user for new input. If they enter something, the while loop runs again.
-                input = JOptionPane.showInputDialog(null, "Do you want to run the program again? If so, please enter a new number to convert.\n\n" + "Inches:", TITLE, JOptionPane.QUESTION_MESSAGE);
-                
             }
         }
 
